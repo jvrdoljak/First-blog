427f0852c98e2c998489803e706cd82041ffcebc
Josip Vrdoljak <vrdoljakjosip@outlook.com>
2018-03-27 15:14:22 +0200

    Little change for test

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,7 +7,9 @@
 <div class="row">
 	<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
 		<h1>Create New Post</h1>
+		<p>This post will inserted in database<br></p>
 		<hr>
+		<!-- Creating form -->
 		{!! Form::open(['route' => 'posts.store']) !!}
     	{{ Form::label('title', 'Title:') }}
     	{{ Form::text('title', null, array('class' => 'form-control')) }}
